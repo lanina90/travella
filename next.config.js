cae7ceae30ee6eae6b054bdfbb0ed64015d779a0
katerynalanina <lanina.kateryna@gmail.com>
2024-08-23 21:53:45 +0300

    first commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+import path from 'path';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+};
+
+export default nextConfig;
